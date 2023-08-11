4699ce8d63e40ccc1d13545699b9db3b2414fc7f
Alexey Groshev <82907949+walkerman-on@users.noreply.github.com>
2023-08-11 20:34:09 +0300

    Making Account page

--- src/components/pages/Account/UserAccount.jsx
+++ src/components/pages/Account/UserAccount.jsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
 import cl from "./UserAccount.module.css"
 import Input from "../../UI/input/Input"
 import Select from "../../UI/select/Select"
@@ -6,12 +7,34 @@ import ReqButton from "../../UI/button/ReqButton/ReqButton"
 import Navigation from "../../navigation/Navigation"
 
 const UserAccount = () => {
+	const [usersInfo, setUsersInfo] = useState([])
+
+	const [userInfo, setUserInfo] = useState({
+		name: "",
+		surname: "",
+		id: "",
+		post: "",
+	})
+
+	const editUserInfo = () => {
+		setUsersInfo([...usersInfo, { ...userInfo, id: Date.now() }])
+		console.log(usersInfo)
+		setUserInfo({
+			name: "",
+			surname: "",
+			userId: "",
+			post: "",
+		})
+	}
+
 	return (
 		<div>
 			<Navigation>Личные данные</Navigation>
 			<div className={cl.navMenu}>
-				<div>Меню выбора</div>
 				<div className={cl.container}>
+					<div className={cl.cont}>
+						<span className={cl.status}>Настройка профиля</span>
+					</div>
 					<div className={cl.userContainer}>
 						{/* <img
 							className={cl.userAvatar}
@@ -25,19 +48,33 @@ const UserAccount = () => {
 								alt="Аватар пользователя"
 							/>
 						</div>
-						<div className={cl.userInfo}>
-							<span className={cl.userNameText}>Иван Абрамов</span>
-							<span className={cl.userIDText}>ID: 234980197</span>
-						</div>
+						{usersInfo.map((info) => (
+							<div className={cl.userInfo}>
+								<span className={cl.userNameText}>
+									{info.name} {info.surname}
+								</span>
+								<span className={cl.userIDText}>ID: {info.userId}</span>
+							</div>
+						))}
 					</div>
 					<div className={cl.infoBlock}>
 						<div className={cl.inputBlock}>
 							<p className={cl.secondaryText}>Имя:</p>
-							<Input></Input>
+							<Input
+								value={userInfo.name}
+								onChange={(e) =>
+									setUserInfo({ ...userInfo, name: e.target.value })
+								}
+							></Input>
 						</div>
 						<div className={cl.inputBlock}>
 							<p className={cl.secondaryText}>Фамилия:</p>
-							<Input></Input>
+							<Input
+								value={userInfo.surname}
+								onChange={(e) =>
+									setUserInfo({ ...userInfo, surname: e.target.value })
+								}
+							></Input>
 						</div>
 					</div>
 					<div className={cl.infoBlock}>
@@ -54,8 +91,34 @@ const UserAccount = () => {
 						</div>
 						<div className={cl.inputBlock}>
 							<p className={cl.secondaryText}>Номер табельного:</p>
-							<Input placeholder="ХХХ ХХХ ХХХ"></Input>
+							<Input
+								placeholder="ХХХ ХХХ ХХХ"
+								value={userInfo.userIid}
+								onChange={(e) =>
+									setUserInfo({ ...userInfo, userId: e.target.value })
+								}
+							></Input>
 						</div>
+					</div>
+					<div className={cl.btnSection}>
+						<ReqButton onClick={editUserInfo}>Сохранить</ReqButton>
+					</div>
+				</div>
+				<div className={cl.container}>
+					<div className={cl.cont}>
+						<span className={cl.status}>Изменение пароля</span>
+					</div>
+					<div className={cl.inputBlock}>
+						<p className={cl.secondaryText}>Текущий пароль:</p>
+						<Input placeholder="*******"></Input>
+					</div>
+					<div className={cl.inputBlock}>
+						<p className={cl.secondaryText}>Новый пароль:</p>
+						<Input placeholder="*******"></Input>
+					</div>
+					<div className={cl.inputBlock}>
+						<p className={cl.secondaryText}>Повторение нового пароля:</p>
+						<Input placeholder="*******"></Input>
 					</div>
 					<div className={cl.btnSection}>
 						<ReqButton>Сохранить</ReqButton>
